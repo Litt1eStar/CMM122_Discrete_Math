0ca0c3de358c3edfb745adfc440720e349b26844
Woody <devkritt@gmail.com>
2025-02-08 15:35:48 +0700

    create logic for create sets from user input

--- Project/HW1_Caculator/set_operations.py
+++ Project/HW1_Caculator/set_operations.py
@@ -12,7 +12,8 @@ def set_menu():
         choice = get_choice(1, 5)
         if choice == 5:
             break
-
+        
+        sets = create_set()
         if choice == 1:
             set_union()
         elif choice == 2:
@@ -33,3 +34,21 @@ def set_difference():
 
 def set_complement():
     print("Performing Set Complement...")
+
+def create_set():
+    sizeA = int(input("Size of Set A: "))
+    sizeB = int(input("Size of Set B: "))
+    
+    setA = {*{}}
+    setB = {*{}}
+    
+    for i in range(sizeA):
+        val = int(input(f"Element {i} | Value : "))
+        setA.add(val)
+        
+    for i in range(sizeB):
+        val = int(input(f"Element {i} | Value : "))
+        setB.add(val)
+        
+    print(setA)
+    print(setB)
