a1164cc18fb3c82015b5c38c2f90e9dc6447cddb
Woody <devkritt@gmail.com>
2025-02-09 16:07:40 +0700

    create: logic for vector addition

--- Project/HW1_Caculator/vector_operation.py
+++ Project/HW1_Caculator/vector_operation.py
@@ -33,8 +33,12 @@ def vector_menu():
             vector_normalization()
             
 def vector_addition():
-    return
-
+    vectors = create_two_vector()
+    vector_a = vectors[0]
+    vector_b = vectors[1]
+    result_vector = [vector_a[0]+vector_b[0], vector_a[1]+vector_b[1], vector_a[2]+vector_b[2]]
+    print(result_vector)
+    
 def vector_subtraction():
     return
 
